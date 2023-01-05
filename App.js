e0b18cd120872330900769731ee534607fe754c2
bange-git <sebastienbange@gmail.com>
2023-01-05 17:51:13 +0100

    added notification for both android and ios

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  const notifHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Your daily remainder!",
+        body: "The way of God is perfect and pure!!!",
+       // data: { userName: "max" },
+      },
+      trigger: {
+        seconds: 5,
+      },
+    });
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="tap for notifications" onPress={notifHandler} />
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
